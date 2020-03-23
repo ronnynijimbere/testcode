95fe46fe2dba36b54e3f46220ca7e43b6556055e
Ronny <ronny.nijimbere86@gmail.com>
2020-03-23 08:54:16 +0200

    header component and Tests

---
+++ src/component/header/spec.test.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Header from './index'
+import {  findByTestAtrr} from './../../../utils/'
+
+//setup
+const setUp = (props={}) => {
+    const component = shallow(<Header {...props} />);
+    return component;
+};
+
+
+
+describe('Header Component', () => {
+
+    let component;
+    // beforeEach runs before every single test
+    beforeEach(() => {
+        component = setUp();
+    });
+
+    it('Should render without errors', () => {
+        const wrapper = findByTestAtrr(component, 'headerComponent');
+        expect(wrapper.length).toBe(1)
+    })
+
+    it('Should render a logo', () => {
+        const logo = findByTestAtrr(component, 'logoIMG');
+        expect(logo.length).toBe(1)
+    })
+
+    //nested example below
+    // describe('Nested', () => {
+    //     it('It should render without errors', () => {
+    //        expect(1).toBe(3); 
+    //     })
+    // })
+})
+
